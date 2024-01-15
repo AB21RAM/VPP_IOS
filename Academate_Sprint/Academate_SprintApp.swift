c38a257f093f4867457946c9e489a373feed64ba
Atharv_bhosale <linux.atharv.bhosale@gmail.com>
2024-01-15 15:21:49 +0530

    Initial Commit

---
+++ Academate_Sprint/Academate_SprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  Academate_SprintApp.swift
+//  Academate_Sprint
+//
+//  Created by Sanjaykumar Bhosale  on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Academate_SprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Academate_Sprint/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Academate_Sprint
+//
+//  Created by Sanjaykumar Bhosale  on 15/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
